b882c4dd55cda9763908b281c1dadc4acb9822bf
Paulo Miguel <paulomigueldasilva@hotmail.com>
2024-04-21 15:16:04 -0300

    Entidades basicas
    primeiro commit de entidades basicas

--- src/main/java/com/example/servicessystem/entidades/SistemaPrestacaoServico.java
+++ src/main/java/com/example/servicessystem/entidades/SistemaPrestacaoServico.java
@@ -1,4 +1,31 @@
 package com.example.servicessystem.entidades;
 
 public class SistemaPrestacaoServico {
+    private Cliente[] clientes;
+    private PrestadorDeServico[] prestadorServico;
+    private Servico[] servicos;
+
+    public SistemaPrestacaoServico(Cliente[] clientes, PrestadorDeServico[] prestadorServicos, Servico[] servico) {
+        this.clientes = clientes;
+        this.prestadorServico = prestadorServicos;
+        this.servicos = servico;
+    }
+
+    public SistemaPrestacaoServico(int tamanhoClientes, int tamanhoPrestadores, int tamanhoServicos) {
+        clientes = new Cliente[tamanhoClientes];
+        prestadorServico = new PrestadorDeServico[tamanhoPrestadores];
+        servicos = new Servico[tamanhoServicos];
+    }
+
+    public void adicionarCliente(Cliente cliente, int index) {
+        clientes[index] = cliente;
+    }
+
+    public void adicionarPrestadorServico(PrestadorDeServico prestador, int index) {
+        prestadorServico[index] = prestador;
+    }
+
+    public void adicionarServico(Servico servico, int index) {
+        servicos[index] = servico;
+    }
 }
