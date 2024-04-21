e5eb32c5e5858db7ae52a188d62c36de3c2ea116
wendellvsilva <wendellcrtss@gmail.com>
2024-04-21 14:40:41 -0300

    Initial commit

--- src/main/java/com/example/servicessystem/entidades/Servico.java
+++ src/main/java/com/example/servicessystem/entidades/Servico.java
@@ -1,5 +1,7 @@
 package com.example.servicessystem.entidades;
 
 public class Servico {
-
+    private String nome;
+    private String Descricao;
+    private float preco; //também pode ser double
 }
